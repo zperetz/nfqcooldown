04a35cf15ae129342affecfd814014acfad700fd
zperetz <90855583+zperetz@users.noreply.github.com>
2026-06-30 16:57:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ trap 'rm -rf "$TMP_DIR"' EXIT
 info "detected platform: ${GOOS}/${GOARCH}"
 info "downloading latest release: ${ASSET}"
 
-curl -qfL "$DOWNLOAD_URL" -o "${TMP_DIR}/${ASSET}" \
+curl -fsSL "$DOWNLOAD_URL" -o "${TMP_DIR}/${ASSET}" \
   || fail "failed to download ${DOWNLOAD_URL}"
 
 info "extracting"
